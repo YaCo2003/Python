49e2688ce5fd6a670af26798115bf8443ce2efca
ye-zhizhi <3020936216@qq.com>
2023-11-11 00:41:29 +0800

    11.11练习

---
+++ 代码练习/11.11.py
@@ -0,0 +1,11 @@
+# t1=('周杰伦',11,['football','music'])
+# print(t1)
+# t1[2].remove('football')
+# print(t1)
+# t1[2].append('coding')
+# print(t1)
+
+str = "itheima itcast boxuegu"
+print(f"字符串{str}中有：{str.count('it')}个it字符")
+print(f"字符串{str}，被替换空格后，结果{str.replace(' ', '|')}")
+print(f"字符串{str}，按照|分隔后，得到{str.split('|')}")
