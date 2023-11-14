1341bbe2d897146d14f6d675342200fe1879bb1a
ye-zhizhi <3020936216@qq.com>
2023-11-14 13:17:54 +0800

    11.13练习

--- 代码练习/11.11.py
+++ 代码练习/11.11.py
@@ -1,11 +1,21 @@
-# t1=('周杰伦',11,['football','music'])
+# t1=('周.杰.伦',11,['football','music'])
 # print(t1)
-# t1[2].remove('football')
+# t1[2].append()
 # print(t1)
-# t1[2].append('coding')
-# print(t1)
+# print(t1[0].split('.'))
+# str = "itheima itcast boxuegu"
+# print(f"字符串{str}中有：{str.count('it')}个it字符")
+# print(f"字符串{str}，被替换空格后，结果{str.replace(' ', '|')}")
+# print(f"字符串{str}，按照|分隔后，得到{str.split('|')}")
 
-str = "itheima itcast boxuegu"
-print(f"字符串{str}中有：{str.count('it')}个it字符")
-print(f"字符串{str}，被替换空格后，结果{str.replace(' ', '|')}")
-print(f"字符串{str}，按照|分隔后，得到{str.split('|')}")
+# str = "itheima"
+# print(f"{str[:]}")
+# print(f"{str[3:1:-1]}")
+
+# str="万过薪月，员序程马黑来，nohtyP学"
+# print(f"{str[9:4:-1]}")
+# my_str=str.split("，")[1].replace('来','')[::-1]
+# print(my_str)
+# str1=str.split(",")[0]="j"
+# print(str1)
+
